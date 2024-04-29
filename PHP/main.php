d09bf4e33d15716829ba439e2ef4e2f176a62b64
hugo <hugo.crneau@gmail.com>
2024-04-29 14:12:56 +0200

    a

--- PHP/main.php
+++ PHP/main.php
@@ -1,21 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
     $name = $_POST["name"];
     $email = $_POST["email"];
     $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    // Adresse e-mail de destination
     $to = "hugo.creneau@etu.uca.fr";
 
-    // Sujet de l'e-mail
     $email_subject = "New Contact Form Submission: $subject";
 
-    // Contenu de l'e-mail
     $email_body = "You have received a new message from $name ($email):\n\n$message";
 
-    // En-têtes de l'e-mail
     $headers = "From: $name <$email>\r\n";
     $headers .= "Reply-To: $email\r\n";
 
