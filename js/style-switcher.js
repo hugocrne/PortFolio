0eb5ea1402d5135061ac0c51813a4aa015bde9ce
hugo <hugo.creneau@etu.uca.fr>
2024-06-09 10:25:49 +0200

    MAJ du portfolio

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,43 +1,41 @@
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggle");
 styleSwitcherToggle.addEventListener("click", () => {
-    document.querySelector(".style-switcher").classList.toggle("open");
-})
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
 
 window.addEventListener("scroll", () => {
-    if (document.querySelector(".style-switcher").classList.contains("open"))
-    {
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
-})
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
 
 const alternateStyle = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color)
-{
-    alternateStyle.forEach((style) => {
-        if (color === style.getAttribute("title"))
-        {
-            style.removeAttribute("disabled");    
-        }
-        else
-        {
-            style.setAttribute("disabled","true");
-        }
-    })
+function setActiveStyle(color) {
+  alternateStyle.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
 }
 
 const dayNight = document.querySelector(".day-night");
+
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");  
-    dayNight.querySelector("i").classList.toggle("fa-moon"); 
-    document.body.classList.toggle("dark");
-})
+  const icon = dayNight.querySelector("em");
+  icon.classList.toggle("fa-sun");
+  icon.classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+
 window.addEventListener("load", () => {
-    if (document.body.classList.contains("dark"))
-    {
-        dayNight.querySelector("i").classList.add("fa-sun");    
-    }
-    else
-    {
-        dayNight.querySelector("i").classList.add("fa-moon");    
-    }
-})
+  const icon = dayNight.querySelector("em");
+  if (document.body.classList.contains("dark")) {
+    icon.classList.add("fa-sun");
+    icon.classList.remove("fa-moon");
+  } else {
+    icon.classList.add("fa-moon");
+    icon.classList.remove("fa-sun");
+  }
+});
